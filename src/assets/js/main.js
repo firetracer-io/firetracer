1e47a2fbfc1e9b801ae6ae06376679438b64d27a
Welpike <106405084+Welpike@users.noreply.github.com>
2022-11-02 21:46:47 +0100

    Update main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,10 @@ function showIframeData(){
     const iframeData = document.getElementById("iframe-data");
     iframeData.classList.toggle("d-none");
 }
+
+copy_embed_api=(e,rcs_type,rcs_id,rcs_name)=>{
+    e.preventDefault();
+    navigator.clipboard.writeText("<iframe src='https://firetracer.000webhostapp.com/embed.php?rcs="+rcs_type+"&id="+rcs_id+"' width='600' height='300' name='"+rcs_name+" - firetracer' sandbox=''></iframe>").then(() => {
+        alert("code copied");    
+    })
+}
